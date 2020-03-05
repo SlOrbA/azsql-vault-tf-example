81c8a725c1efb96b25d28fc5549d60065f945374
Lari Korpi <lari.korpi@iki.fi>
2020-03-05 14:06:45 +0200

    chagned to support latest AzureRM provided

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-provider "azurerm" {}
+provider "azurerm" {
+  features {}
+}
 
 provider "vault" {
   address = "https://${azurerm_app_service.vault-app.default_site_hostname}"
-  token   = "${random_uuid.root-token.result}"
+  token   = random_uuid.root-token.result
 }
 
 resource "random_uuid" "root-token" {}
@@ -13,7 +15,7 @@ resource "vault_mount" "db" {
 }
 
 resource "vault_database_secret_backend_connection" "azsql" {
-  backend       = "${vault_mount.db.path}"
+  backend       = vault_mount.db.path
   name          = "example"
   allowed_roles = ["dev","prod"]
 
@@ -23,9 +25,9 @@ resource "vault_database_secret_backend_connection" "azsql" {
 }
 
 resource "vault_database_secret_backend_role" "role"{
-  backend               = "${vault_mount.db.path}"
+  backend               = vault_mount.db.path
   name                  = "dev"
-  db_name               = "${vault_database_secret_backend_connection.azsql.name}"
+  db_name               = vault_database_secret_backend_connection.azsql.name
   creation_statements   = ["CREATE USER [{{name}}] WITh PASSWORD = '{{password}}';"]
   revocation_statements = ["DROP USER IF EXISTS [{{name}}]"]
   default_ttl           = "300"
@@ -39,11 +41,11 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_sql_server" "dbserver" {
   name                         = "db-srv-vault-${random_string.app-name.result}-example"
-  resource_group_name          = "${azurerm_resource_group.rg.name}"
-  location                     = "${azurerm_resource_group.rg.location}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "${random_string.admin-username.result}"
-  administrator_login_password = "${random_password.admin-password.result}"
+  administrator_login          = random_string.admin-username.result
+  administrator_login_password = random_password.admin-password.result
 }
 
 resource "random_string" "admin-username" {
@@ -62,23 +64,23 @@ resource "random_password" "admin-password" {
 
 resource "azurerm_sql_database" "sqldb" {
   name                = "db-example"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  server_name         = "${azurerm_sql_server.dbserver.name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  server_name         = azurerm_sql_server.dbserver.name
 }
 
 resource "azurerm_sql_firewall_rule" "vault" {
   name                = "vault-access"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  server_name         = "${azurerm_sql_server.dbserver.name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_sql_server.dbserver.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
 
 resource "azurerm_app_service_plan" "vault-plan" {
   name                = "vault-plan-example"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   kind                = "Linux"
   reserved            = true
 
@@ -90,16 +92,16 @@ resource "azurerm_app_service_plan" "vault-plan" {
 
 resource "azurerm_app_service" "vault-app" {
   name                = "vault-app-${random_string.app-name.result}-example"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  location            = "${azurerm_resource_group.rg.location}"
-  app_service_plan_id = "${azurerm_app_service_plan.vault-plan.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  app_service_plan_id = azurerm_app_service_plan.vault-plan.id
 
   site_config {
     linux_fx_version = "DOCKER|vault"
   }
 
   app_settings = {
-    VAULT_DEV_ROOT_TOKEN_ID             = "${random_uuid.root-token.result}"
+    VAULT_DEV_ROOT_TOKEN_ID             = random_uuid.root-token.result
     VAULT_LOCAL_CONFIG                  = "{ \"ui\":  \"true\"}"
     SKIP_SETCAP                         = "yep"
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
